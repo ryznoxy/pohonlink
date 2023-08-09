d136d5265217beffa0a7cbb01f10f311c329d572
rzznoxy <rezajelek2@gmail.com>
2023-08-09 16:34:40 +0700

    adding spotify

---
+++ src/main.js
@@ -0,0 +1,12 @@
+function toggle(){
+  let element = document.getElementById('iframeKu');
+  let hidden = element.getAttribute('hidden');
+
+  if (hidden) {
+    element.removeAttribute('hidden');
+    
+  } else {
+    element.setAttribute('hidden','hidden');
+    
+  }
+}
